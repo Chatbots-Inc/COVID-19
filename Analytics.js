330a1274ca561290ae4246d36f5fc7cf5621605e
DavidGarzaGonzalez <62816728+DavidGarzaGonzalez@users.noreply.github.com>
2020-04-05 14:06:55 -0500

    Update Analytics.js

--- Analytics.js
+++ Analytics.js
@@ -15,13 +15,22 @@ exports.ProyectoDePrueba = functions.https.onRequest((request, response) => {
     console.log('Request headers: ' + JSON.stringify(request.headers));
     console.log('Request body: ' + JSON.stringify(request.body));
 
-    const a = JSON.stringify(request.body.responseId);	// Para obtener el id de respuesta
-    const b = JSON.stringify(request.body.queryResult.intent.displayName);		// Para obtener el intent
-    const c = JSON.stringify(request.body.queryResult.queryText);			// Para obtener la solicitud del usuario
-    const d = JSON.stringify(request.body.queryResult.intentDetectionConfidence);	// Para obtener el parámetro de confianza
+    var a = JSON.stringify(request.body.responseId);	// Para obtener el id de respuesta
+    var b = JSON.stringify(request.body.queryResult.intent.displayName);		// Para obtener el intent
+    var c = JSON.stringify(request.body.queryResult.queryText);			// Para obtener la solicitud del usuario
+    var d = JSON.stringify(request.body.queryResult.intentDetectionConfidence);	// Para obtener el parámetro de confianza
 
-    return admin.database().ref(Num).set({
-        SessId: a,
+    // Limpiamos la primera parte de los datos.
+    a = a.substring(1);
+    b = b.substring(1);
+    c = c.substring(1);
+
+    // Limpiamos la ultima parte de los datos.
+    a = a.slice(0,-1);
+    b = b.slice(0,-1);
+    c = c.slice(0,-1);
+
+    return admin.database().ref(a).set({
         IntentName: b,
         TextQuery: c,
         Confidence: d,
