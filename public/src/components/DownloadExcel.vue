1aff2de122153f39944494156bbc6652d8bea0d0
alextrujillo <alexandro4v@gmail.com>
2020-04-30 16:01:45 -0500

    Dashboard Deploy

---
+++ public/src/components/DownloadExcel.vue
@@ -0,0 +1,36 @@
+<template>
+    <download-excel
+        :data   = users
+        :fields = "json_fields"
+        worksheet = "My Worksheet"
+        name    = "filename.xls">
+        <b-button variant="success">Descargar Excel</b-button>
+    </download-excel>
+</template>
+
+<script>
+import { usersRef } from '../firebase';
+
+export default {
+  data() {
+    return {
+      users: [],
+      json_fields: {
+        'TextQuery': 'TextQuery',
+        'IntentName': 'IntentName',
+        'Confidence': 'Confidence',
+        'responseId': 'responseId',
+      },
+    };
+  },
+  firebase: {
+    users: usersRef,
+  },
+};
+</script>
+
+<style scoped>  /* "scoped" attribute limit the CSS to this component only */
+  .download-excel {
+    margin-top: 30px;
+  }
+</style>
---
+++ public/src/components/UsersTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <b-table striped hover :items="users_data" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import { usersRef } from '../firebase';
+
+moment.locale('es-us');
+
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: 'TextQuery',
+          label: 'TextQuery',
+          sortable: false,
+        },
+        {
+          key: 'IntentName',
+          label: 'IntentName',
+          sortable: true,
+        },
+        {
+          key: 'Confidence',
+          label: 'Confidence',
+          sortable: false,
+        },
+        {
+          key: 'responseId',
+          label: 'responseId',
+          sortable: false,
+        },
+
+      ],
+    };
+  },
+  firebase: {
+    users: usersRef,
+  },
+  computed: {
+    users_data() {
+      return this.users.map((user) => ({
+        ...user,
+        Confidence: user.Confidence,
+        TextQuery:user.TextQuery,
+        IntentName:user.IntentName,
+        responseId:user.responseId
+      }));
+    },
+  },
+
+};
+
+</script>
+
+
+<style scoped>  /* "scoped" attribute limit the CSS to this component only */
+
+  .my-panel {
+    margin:32px;
+  }
+
+</style>
