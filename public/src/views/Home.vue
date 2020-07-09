e4205c5b478fa3a81bd77f2b45c4a483152fcccb
vivi m <vivianamaldes@gmail.com>
2020-07-09 11:59:54 -0500

    Initial download dialog

--- public/src/views/Home.vue
+++ public/src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <b-button variant="outline-danger" @click="logout">Logout</b-button>
+    <datepicker placeholder="Seleccionar fecha inicial"></datepicker>
+    <datepicker placeholder="Seleccionar fecha final"></datepicker>
     <DownloadExcel class="download-sorted-excel" sortBy="SessionId" sortName="sesión"/>
-    <DownloadExcel class="download-sorted-excel" sortBy="Time" sortName="fecha"/>
+    <DownloadExcel class="download-sorted-excel" sortBy="Name" sortName="nombre" nameList="Dr PhD Jose Luis Gonzalez"/>
+    <DownloadExcel class="download-sorted-excel" sortBy="Time" sortName="fecha" startTime="1593900773580" endTime="1594069811832"/>
     <UsersTable class="users-table"/>
   </div>
 
